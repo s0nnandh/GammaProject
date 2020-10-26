c03fafd891de26fc1a3d4a2b4f023cb5e1373a95
Sonnandh <190050055@iitb.ac.in>
2020-10-26 10:08:45 +0530

    modified views

--- project/phaseone/views.py
+++ project/phaseone/views.py
@@ -5,18 +5,12 @@ from .forms import TempForm
 from .models import MessageForm
 from django.contrib import messages
 
-# Create your views here.
-
-proxy_dict = {
-          "http"  : "http://127.0.0.1",
-          "https" : "http://127.0.0.1",
-        }
 
 def home_view(request):
     form = TempForm(request.POST or None, request.FILES or None)
     if request.method =='POST':         
-        if form.is_valid():
-            notify(form)
+        if form.is_valid():            
+            notify(form.cleaned_data)
             form.save()              
             form = TempForm() 
             messages.success(request, "Successfully created") 
@@ -24,10 +18,12 @@ def home_view(request):
     return render(request,"phaseone/home.html",{'form' : form})
 
 def notify(form):
+    print('a')
     push_service = FCMNotification(api_key="AAAAvHIUUss:APA91bElas2wl0uWdjmnQimvMQBgYX2XpFr75ilust04cMLFzbe04eoNSPMK-3wV8DAMhgX8hvQ0LGyEhw4sCzSFY0D3abUEVZM8BBy6yhPTViO_f35LJaBwgdjFCio0Y9bOq-sSnNfI")
-    registeration_id = "euqOvvdDSKI:APA91bEKoIfxoqhWs6lQ_1FvlGFt6f5JcoPanu7oV2YI9RrEH9IbqScEIdjfC091TKsgIu73afyDaSsN8gzv_aylzb5rOZcekN3j9e_SYJU52O02bmvFxMfnlRLTFmOA6KJdeAzjkyrW"
-    message_title = "Hi"
-    message_body = "Hello"
+    registeration_id = "euqOvvdDSKI:APA91bEKoIfxoqhWs6lQ_1FvlGFt6f5JcoPanu7oV2YI9RrEH9IbqScEIdjfC091TKsgIu73afyDaSsN8gzv_aylzb5rOZcekN3j9e_SYJU52O02bmvFxMfnlRLTFmOA6KJdeAzjkyrW"    
+    message_title = form['header']
+    print(message_title)
+    message_body = form['text']
     result = push_service.notify_single_device(registration_id=registeration_id, message_title=message_title, message_body=message_body)
     print(result)
 
