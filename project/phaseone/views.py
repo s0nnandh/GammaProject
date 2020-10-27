17a6dbac3c3b4ae9afa505c1a9045f87323db020
Sonnandh <190050055@iitb.ac.in>
2020-10-27 22:26:01 +0530

    changed fcm

--- project/phaseone/views.py
+++ project/phaseone/views.py
@@ -18,11 +18,13 @@ def home_view(request):
 def notify(form):
     print('a')
     push_service = FCMNotification(api_key="AAAAvHIUUss:APA91bElas2wl0uWdjmnQimvMQBgYX2XpFr75ilust04cMLFzbe04eoNSPMK-3wV8DAMhgX8hvQ0LGyEhw4sCzSFY0D3abUEVZM8BBy6yhPTViO_f35LJaBwgdjFCio0Y9bOq-sSnNfI")
-    registeration_id = "euqOvvdDSKI:APA91bEKoIfxoqhWs6lQ_1FvlGFt6f5JcoPanu7oV2YI9RrEH9IbqScEIdjfC091TKsgIu73afyDaSsN8gzv_aylzb5rOZcekN3j9e_SYJU52O02bmvFxMfnlRLTFmOA6KJdeAzjkyrW"    
+    registeration_ids = [
+        "euqOvvdDSKI:APA91bEKoIfxoqhWs6lQ_1FvlGFt6f5JcoPanu7oV2YI9RrEH9IbqScEIdjfC091TKsgIu73afyDaSsN8gzv_aylzb5rOZcekN3j9e_SYJU52O02bmvFxMfnlRLTFmOA6KJdeAzjkyrW",
+        "euqOvvdDSKI:APA91bEKoIfxoqhWs6lQ_1FvlGFt6f5JcoPanu7oV2YI9RrEH9IbqScEIdjfC091TKsgIu73afyDaSsN8gzv_aylzb5rOZcekN3j9e_SYJU52O02bmvFxMfnlRLTFmOA6KJdeAzjkyrW"]  
     message_title = form['header']
     print(message_title)
     message_body = form['text']
-    result = push_service.notify_single_device(registration_id=registeration_id, message_title=message_title, message_body=message_body)
+    result = push_service.notify_multiple_devices(registration_ids=registeration_id, message_title=message_title, message_body=message_body)
     print(result)
 def null(request):
     return redirect("home/")
